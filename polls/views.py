68bd8a5d9c8265ab45ecfed1da10b2063975f538
jarralKeshav <keshavjrall@outlook.com>
2023-10-06 21:30:25 +0530

    6th oct

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello Polls Index.")
+    # return HttpResponse("Hello Polls Index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ",".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("Your're looking at question %s." %question_id)
+
+def results(request, question_id):
+    response = "You're looking ar the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
 
 
